2c31d17a57a872d08e03ea5b40af7c982455fb51
Toby Crawley <toby@tcrawley.org>
2014-03-31 17:04:24 -0400

    Bump version to 0.1.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-webdav "0.1.0"
+(defproject lein-webdav "0.1.1-SNAPSHOT"
   :description "A simple plugin that enables deploying to webdav repos."
   :url "https://github.com/tobias/lein-webdav"
   :license {:name "Eclipse Public License"
